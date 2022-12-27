407d5d36002ece09085bf31deac514e69d69d653
Aggelos Tzimopoulos <agtzim@gmail.com>
2022-12-27 13:18:35 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,41 +1,50 @@
--- My init.lua
-
+-- My init file
+-- Install packer
 vim.g.mapleader = ' '
-vim.opt.number = true
-vim.opt.hlsearch = false
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.guifont = 'Cascadia Code PL:h11'
+vim.g.maplocalleader = ' '
+vim.o.guifont = 'Cascadia Code PL:h11'
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.number = true
 
 require('packer').startup({ function(use)
-	-- Packer can manage itself
+
+	-- Package manager
 	use 'wbthomason/packer.nvim'
+
 	use 'folke/tokyonight.nvim'
-	use 'nvim-tree/nvim-web-devicons'
+
+	use 'Pocco81/auto-save.nvim'
 
 	use {
 		'nvim-treesitter/nvim-treesitter',
-		run = function()
-			require('nvim-treesitter.install').update({ with_sync = true })()
-		end,
+		run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
 	}
 
 	use 'windwp/nvim-autopairs'
 
-	use "folke/neodev.nvim"
+	use "lukas-reineke/indent-blankline.nvim"
 
-	use {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
+	use { -- LSP Configuration & Plugins
+		'neovim/nvim-lspconfig',
+		requires = {
+			-- Automatically install LSPs to stdpath for neovim
+			'williamboman/mason.nvim',
+			'williamboman/mason-lspconfig.nvim',
+
+			-- Useful status updates for LSP
+			--'j-hui/fidget.nvim',
+
+			-- Additional lua configuration, makes nvim stuff amazing
+			'folke/neodev.nvim',
+		},
 	}
 
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'hrsh7th/cmp-buffer'
-	use 'hrsh7th/nvim-cmp'
-
-	use 'L3MON4D3/LuaSnip'
-	use 'saadparwaiz1/cmp_luasnip'
+	-- Completion
+	use {
+		'hrsh7th/nvim-cmp',
+		requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+	}
 
 end,
 	config = {
@@ -44,15 +53,20 @@ end,
 				return require('packer.util').float({ border = 'single' })
 			end
 		}
+
 	}
 })
 
 
-require('tokyonight').load()
+vim.cmd.colorscheme('tokyonight')
+
+
+require('auto-save').setup {}
+
 
 require 'nvim-treesitter.configs'.setup {
 	-- A list of parser names, or "all"
-	ensure_installed = { "lua", "typescript" },
+	ensure_installed = { "lua", "help" },
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
@@ -70,14 +84,9 @@ require 'nvim-treesitter.configs'.setup {
 	highlight = {
 		-- `false` will disable the whole extension
 		enable = true,
-
-		-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-		-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-		-- the name of the parser)
-		-- list of language that will be disabled
-		-- disable = { "c", "rust" },
 		-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
 		disable = function(lang, buf)
+			print(lang)
 			local max_filesize = 100 * 1024 -- 100 KB
 			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
 			if ok and stats and stats.size > max_filesize then
@@ -90,70 +99,46 @@ require 'nvim-treesitter.configs'.setup {
 		-- Using this option may slow down your editor, and you may see some duplicate highlights.
 		-- Instead of true it can also be a list of languages
 		additional_vim_regex_highlighting = false,
-	},
+	}
 }
 
 
+
+-- TODO - Learn more about autopairs configuration
 require('nvim-autopairs').setup({
-	check_ts = true,
-	ts_config = {
-		lua = { 'string' }, -- it will not add a pair on that treesitter node
-	}
+	check_ts = true
 })
+
+
+require("indent_blankline").setup {
+	-- for example, context is off by default, use this to turn it on
+	show_current_context = true,
+	show_current_context_start = true,
+}
 
 
 require('neodev').setup()
-
-
-require("mason").setup()
-require("mason-lspconfig").setup()
-
-
-local cmp = require('cmp')
-cmp.setup({
-	snippet = {
-		-- REQUIRED - you must specify a snippet engine
-		expand = function(args)
-			require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-		end,
-	},
-	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
-		-- { name = 'luasnip' }, -- For luasnip users.
-	}, {
-		{ name = 'buffer' },
-	})
-})
-
-
-require('lspconfig').sumneko_lua.setup {
-	capabilities = require('cmp_nvim_lsp').default_capabilities(),
-	settings = {
-		Lua = {
-			runtime = {
-				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-				version = 'LuaJIT',
-			},
-			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = { 'vim' },
-			},
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
-				checkThirdParty = false
-			},
-			-- Do not send telemetry data containing a randomized but unique identifier
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
+require('mason').setup()
+require('mason-lspconfig').setup {
+	ensure_installed = { 'sumneko_lua' }
 }
 
-require('lspconfig').tsserver.setup {}
+
+local lspconfig = require('lspconfig')
+
+lspconfig.sumneko_lua.setup {
+	settings = {
+		Lua = {
+			workspace = {
+				checkThirdParty = false
+			},
+			telemetry = {
+				enable = false
+			}
+		}
+	}
+}
 
 
------------------- Keymaps --------------------
+-- KEYMAPS
 vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
